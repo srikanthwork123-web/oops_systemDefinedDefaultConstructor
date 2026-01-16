2985353450a9efd0b1a365c13d5183a73c67052a
Dell <Dell@DESKTOP-13B42NJ>
2026-01-16 08:54:50 +0530

    Add project files.

---
+++ oops_systemDefinedDefaultConstructor/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oops_systemDefinedDefaultConstructor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            myclass mc = new myclass();
+            mc.Display();
+            Console.ReadLine();
+        }
+    }
+}
+/*
+ *output:
+a value before assignment:0
+b value after assignment:0
+a is:10
+b is:20
+*/
---
+++ oops_systemDefinedDefaultConstructor/myclass.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oops_systemDefinedDefaultConstructor
+{
+    internal class myclass
+    {
+        int a;
+        int b;
+        //system defined default constructor compliler will assign some intial values to varaibles nased on datatype.
+        //below is user defined default constructor created by programmer.here you can assign your customized values.
+        public myclass()
+        {
+            Console.WriteLine("a value before assignment:" + a);
+            Console.WriteLine("b value after assignment:" + b);
+            a = 10;
+            b = 20;
+        }
+        public void Display()
+        {
+        
+            Console.WriteLine("a is:" + a);
+            Console.WriteLine("b is:" + b);
+        }
+    }
+}
