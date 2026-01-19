177ab6301df04ed21cdc9eeec2ef5e3717988d0d
Dell <Dell@DESKTOP-13B42NJ>
2026-01-19 07:32:43 +0530

    namining conventions updated

--- oops_systemDefinedDefaultConstructor/myclass.cs
+++ oops_systemDefinedDefaultConstructor/myclass.cs
@@ -7,8 +7,9 @@ using System.Threading.Tasks;
 namespace oops_systemDefinedDefaultConstructor
 {
     internal class myclass
-    {  //instance members start here(variables+methods)=>we called as instance members
-        int a;//we called this variables as "instance variables"
+    { //instancemembers(instancevariables+instance methods)
+        //instance members start here(variables+methods)=>we called as instance members
+        int a;//we called this variables as "instance variables" or normal variables or non static variables
         int b;
         //system defined default constructor compliler will assign some intial values to varaibles nased on datatype.
         //below is user defined default constructor created by programmer.here you can assign your customized values.
@@ -19,7 +20,7 @@ namespace oops_systemDefinedDefaultConstructor
             a = 10;
             b = 20;
         }
-        public void Display()//we called this method as  //instance method
+        public void Display()//we called this method as  normal method (or)  //instance method (or) non static method
         {
         
             Console.WriteLine("a is:" + a);
